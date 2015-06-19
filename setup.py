0ae50f681d4bcacb49d34047e9a10bed10829356
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2015-06-19 15:47:13 +0300

    Add setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-    path.to.file
-    ~~~~~~~~~~~~
+from setuptools import setup
 
-    Short description.
 
-    :copyright: (c) 2015 by Rambler&Co.
-"""
+setup(
+    name='python-ponominalu',
+    version='0.0.1',
+    url='https://github.com/abogoyavlensky/python-ponominalu',
+    license='MIT',
+    author='Andrey Bogoyavlensky',
+    author_email='abogoyavlensky@gmail.com',
+    description='Python client for Ponominalu API.',
+    platforms='any',
+    install_requires=[
+        'requests',
+    ],
+    keywords='rambler ponominalu api',
+)
