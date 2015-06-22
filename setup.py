ae1e94d91ffb51127d16572c781bc1dbbe492708
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2015-06-22 10:19:11 +0300

    Updtae metainfo for package and upgrade version.

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-ponominalu',
-    version='0.0.2',
+    version='0.0.4',
     url='https://github.com/abogoyavlensky/python-ponominalu',
     license='MIT',
     author='Andrey Bogoyavlensky',
     author_email='abogoyavlensky@gmail.com',
     description='Python client for Ponominalu API.',
+    long_description='Python client for Ponominalu API.',
     platforms='any',
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
-    keywords='rambler ponominalu api',
+    keywords='ponominalu',
 )
